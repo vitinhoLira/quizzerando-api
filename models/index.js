9e2d8c0bf06a7852b06be4ee0d8d9e8a93d973b6
João Vitor <contato.joaovitorlira@hotmail.com>
2025-05-09 01:32:00 -0300

    ajuste no arquivo index em models na criação das entidades.

--- models/index.js
+++ models/index.js
@@ -3,11 +3,16 @@ const Sequelize = require('sequelize');
 
 const Quizz = require('./quizz');
 
+const Pergunta = require('./pergunta');
+
 const quizz = Quizz(sequelize, Sequelize.DataTypes);
+
+const pergunta = Pergunta(sequelize, Sequelize.DataTypes);
 
 const db = {
 
     quizz,
+    pergunta,
     sequelize
 
 }
