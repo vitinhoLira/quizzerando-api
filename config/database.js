ef96854b6360c375ac251a53327b20b45a587619
João Vitor <contato.joaovitorlira@hotmail.com>
2025-05-20 00:06:15 -0300

    ajuste

--- config/database.js
+++ config/database.js
@@ -1,6 +1,12 @@
 module.exports = {
 
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DATABASE,
