0a4dd7212881f00d14418c43961e92d0d7370b34
João Vitor <contato.joaovitorlira@hotmail.com>
2025-05-19 23:11:48 -0300

    ajuste de porta

--- app.js
+++ app.js
@@ -14,8 +14,10 @@ sequelize.sync().then(()=>{
 
 })
 
-app.listen(process.env.PORT, () => {
+const port = process.env.PORT || 3000;
 
-    console.log('App funcionando: ' + process.env.PORT)
+app.listen(port, () => {
+
+    console.log('App funcionando: ' + port)
 
 });
