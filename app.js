e9479a0bcbfeb09e50312bb6692e466942b174dc
João Vitor <contato.joaovitorlira@hotmail.com>
2025-05-23 16:59:05 -0300

    config cors

--- app.js
+++ app.js
@@ -7,11 +7,22 @@ const {sequelize} = require('./models');
 
 const app = express();
 
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
+
 app.use(express.json());
 
 app.use('/', routers);
 
-app.use(cors());
+
+
+
 
 sequelize.sync().then(()=>{
 
@@ -19,7 +30,7 @@ sequelize.sync().then(()=>{
 
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
 
